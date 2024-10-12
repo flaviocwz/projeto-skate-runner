5f1928f67165fe00ef4b82ff580534466ffffd27
Mizael Paris <mizael.leite@dcomp.ufs.br>
2024-10-12 14:09:06 -0300

    criando função para parar o jogo ao bater em um obstáculo

--- javascript/main.js
+++ javascript/main.js
@@ -1,17 +1,30 @@
 const skatista = document.querySelector('.skatista');
+const lata = document.querySelector('.lata');
 
 
-const jump = () => {
-   
-    
-        skatista.classList.add('jump');
-
+const jump = () => {  // Função responsável por fazer o personagem pular 
         
+        skatista.classList.add('jump');
         setTimeout(() => {
             skatista.classList.remove('jump');
         }, 500);
     }
 
+const over = setInterval(() => { // Função para parar a animação quando o skatista bater na lata de lixo
 
+    const lataPosition = lata.offsetLeft;
+    const skatistaPosition = +window.getComputedStyle(skatista).bottom.replace('px', '');
+
+    if (lataPosition <= 75 && lataPosition > 0 && skatistaPosition < 68) {
+        lata.style.animation = 'none'
+        lata.style.left = `${lataPosition}px`;
+
+        skatista.style.animation = 'none';
+        skatista.style.bottom = `${skatistaPosition}px`;
+
+        clearInterval(over)
+    }
+
+}, 10);
 
 document.addEventListener('keydown', jump);
