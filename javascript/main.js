d51a2695fbb13e6d8f85c8c7e297999f958d7c3c
Mizael Paris <mizael.leite@dcomp.ufs.br>
2024-10-11 23:27:10 -0300

    aprimorando o index e criando animações básicas

---
+++ javascript/main.js
@@ -0,0 +1,13 @@
+const skatista = document.querySelector('.skatista');
+
+const jump = () => {
+    skatista.classList.add('jump');
+
+    setTimeout(() => {
+       
+    skatista.classList.remove('jump');
+
+    },500);
+}
+
+document.addEventListener('keydown', jump)
