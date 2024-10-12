ee2e8faf5d3e82d156196a64d7b73e179a133c19
flaviocwz <flavio.cruz@dcomp.ufs.br>
2024-10-12 18:09:23 -0300

    main.js
    ajustes básicos

--- javascript/main.js
+++ javascript/main.js
@@ -1,14 +1,12 @@
 const skatista = document.querySelector('.skatista');
 const lata = document.querySelector('.lata');
 
-
 const jump = () => {  // Função responsável por fazer o personagem pular 
-        
-        skatista.classList.add('jump');
-        setTimeout(() => {
-            skatista.classList.remove('jump');
-        }, 500);
-    }
+    skatista.classList.add('jump');
+    setTimeout(() => {
+        skatista.classList.remove('jump');
+    }, 600); // Ajustei o tempo para 600ms, para combinar com a animação CSS
+}
 
 const over = setInterval(() => { // Função para parar a animação quando o skatista bater na lata de lixo
 
@@ -16,13 +14,13 @@ const over = setInterval(() => { // Função para parar a animação quando o sk
     const skatistaPosition = +window.getComputedStyle(skatista).bottom.replace('px', '');
 
     if (lataPosition <= 75 && lataPosition > 0 && skatistaPosition < 68) {
-        lata.style.animation = 'none'
+        lata.style.animation = 'none';
         lata.style.left = `${lataPosition}px`;
 
         skatista.style.animation = 'none';
         skatista.style.bottom = `${skatistaPosition}px`;
 
-        clearInterval(over)
+        clearInterval(over);
     }
 
 }, 10);
